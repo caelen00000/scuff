5af6da28ee21ee4dd28ff0ab807a4ce30c8c9770
caelen00000 <direct_your_inquiry_to_caelen@protonmail.com>
2022-12-30 11:26:23 -0600

    implement basic classes.iastate.edu with Selenium

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from enum import Enum
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.webdriver import WebDriver
 
 
-# Press the green button in the gutter to run the script.
+class Term(Enum):
+    FALL = 1
+    SPRING = 2
+    SUMMER = 3
+
+
+def click_term(driver: WebDriver, term: Term):
+    available_terms = driver.find_element(By.ID, 'semesterId')
+
+    term_element = available_terms.find_elements(By.XPATH, "*")[term.value - 1]
+
+    available_terms.click()
+    term_element.click()
+
+
+def get_subjects(driver: WebDriver):
+    department_chosen = driver.find_element(By.ID, 'department_chosen')
+    department_chosen.click()
+
+    r = department_chosen.find_element(By.CLASS_NAME, 'chosen-results').find_elements(By.XPATH, '*')
+
+    department_chosen.click()  # deselects the dropdown
+
+    return r
+
+
+def click_subject(driver: WebDriver, i):
+    department_chosen = driver.find_element(By.ID, 'department_chosen')
+    department_chosen.click()
+
+    department_chosen.find_element(By.CLASS_NAME, 'chosen-results').find_elements(By.XPATH, '*')[i].click()
+
+
+def click_search_button(driver: WebDriver):
+    driver.find_element(By.ID, 'submit_button').click()
+
+
+def main():
+    driver = webdriver.Firefox()
+    driver.get('https://classes.iastate.edu/')
+
+    click_term(driver, Term.SPRING)
+
+    # subjects = get_subjects(driver)
+
+    click_subject(driver, 116)  # math
+
+    click_search_button(driver)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
